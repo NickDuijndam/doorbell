0f5ecfb7925b53b867f2385a4709b82e7cab9433
Nick Duijndam <nickduijndam@hotmail.com>
2020-06-12 18:25:55 +0200

    Add badge to notification

--- src/models/Subscription.ts
+++ src/models/Subscription.ts
@@ -1,7 +1,7 @@
 import { Column, DataType, DeletedAt, Model, Table, Unique, UpdatedAt } from 'sequelize-typescript';
 import { PushSubscription } from 'web-push';
 
-@Table({ paranoid: false })
+@Table({ paranoid: false, tableName: 'subscriptions' })
 export default class Subscription extends Model<Subscription> {
 
 	@Unique
